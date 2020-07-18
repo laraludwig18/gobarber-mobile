f80cb2bcaf24959f45e12882a5b8cff3a4168750
lara <lara.ludwig@4all.com>
2020-07-18 15:25:10 -0300

    Add splash, icon and change app name

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,37 @@
 import 'react-native-gesture-handler';
 
-import React from 'react';
+import React, { useEffect } from 'react';
 import { View, StatusBar } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+import SplashScreen from 'react-native-splash-screen';
 
 import AppProvider from './context';
 import Routes from './routes';
 
 import { Colors } from './constants';
 
-const App: React.FC = () => (
-  <NavigationContainer>
-    <StatusBar
-      barStyle="light-content"
-      backgroundColor={Colors.BACKGROUND}
-      translucent
-    />
-    <AppProvider>
-      <SafeAreaProvider>
-        <View style={{ flex: 1, backgroundColor: Colors.BACKGROUND }}>
-          <Routes />
-        </View>
-      </SafeAreaProvider>
-    </AppProvider>
-  </NavigationContainer>
-);
+const App: React.FC = () => {
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
+
+  return (
+    <NavigationContainer>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor={Colors.BACKGROUND}
+        translucent
+      />
+      <AppProvider>
+        <SafeAreaProvider>
+          <View style={{ flex: 1, backgroundColor: Colors.BACKGROUND }}>
+            <Routes />
+          </View>
+        </SafeAreaProvider>
+      </AppProvider>
+    </NavigationContainer>
+  );
+};
 
 export default App;
